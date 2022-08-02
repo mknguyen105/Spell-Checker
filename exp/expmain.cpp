a29d22d284cbefef93900b14d4513e668e02486a
mknguyen105 <30991432+mknguyen105@users.noreply.github.com>
2022-08-01 18:56:31 -0700

    Update expmain.cpp

--- exp/expmain.cpp
+++ exp/expmain.cpp
@@ -1,8 +1,5 @@
 // expmain.cpp
 //
-// ICS 46 Spring 2017
-// Project #3: Set the Controls for the Heart of the Sun
-//
 // Do whatever you'd like here.  This is intended to allow you to experiment
 // with your code, outside of the context of the broader program or Google
 // Test.
