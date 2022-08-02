90eca13c0c4e8ed6cc33e4afdc17b198c534fe95
mknguyen105 <30991432+mknguyen105@users.noreply.github.com>
2022-08-01 18:55:45 -0700

    Update WordSetLoader.hpp

--- provided/WordSetLoader.hpp
+++ provided/WordSetLoader.hpp
@@ -1,8 +1,5 @@
 // WordSetLoader.hpp
 //
-// ICS 46 Spring 2017
-// Project #3: Set the Controls for the Heart of the Sun
-//
 // A class that loads a word set from a file containing one word on each
 // line.  The words are then added to the given Set<std::string>.
 
